29967833403c176f4b99c49dcbb57b02263259bb
Kulsgam <kuls.gam@gmail.com>
2024-09-12 11:20:56 +1000

    feat(chatLoop): non-unity compatible version

--- Assets/Scripts/ChatLoop.cs
+++ Assets/Scripts/ChatLoop.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
+using System.Text.Json.Nodes;
+// using DotNetEnv;
+// using IPA;
 
 public class ChatLoop : MonoBehaviour
 {
+    public GroqApiClient groqApi;
+
     void Start()
     {
         Debug.Log("ChatLoop");

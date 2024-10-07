a3514f6f4153645c26ac1f87ee3b9064c14a7ac6
herpichRmit <s3935413@student.rmit.edu.au>
2024-10-07 17:03:53 +1100

    added functionality where user is shown the number of messages remaining. After 10 messages user is able to end the level.

--- Assets/Scripts/ChatLoop.cs
+++ Assets/Scripts/ChatLoop.cs
@@ -5,6 +5,7 @@ using Newtonsoft.Json.Linq;
 using System.Threading.Tasks;
 using System.IO;
 using System.Text.RegularExpressions;
+using TMPro;
 
 public class ChatLoop : MonoBehaviour
 {
@@ -15,6 +16,8 @@ public class ChatLoop : MonoBehaviour
     public string chatLogFilePath;
     public GameObject loadingSymbol;
     public int msgsSent { get; private set; } = 0;
+    public TextMeshProUGUI messagesRemainingValue;
+    public LevelManagement levelManagement;
 
     private static readonly string initialAIMessage = "Hello, welcome to our cafe. What can I get for you today?";
     //private static readonly OnboardingData onboardingData = new OnboardingData();
@@ -194,7 +197,17 @@ public class ChatLoop : MonoBehaviour
         await AIVoice.Speak(contentStr);
         setIsResponding(false);
 
+        // Update the number of messages remaining
         ++msgsSent;
+        int temp = 10 - msgsSent;
+        messagesRemainingValue.text = temp.ToString();
+
+        // When max number of messages have been set, switch to 'finished level' UI
+        if (msgsSent == 10)
+        {
+            levelManagement.switchDisplays();
+        }
+        
     }
 
     private bool GetSentiment(string msg)
--- Assets/Scripts/LevelManagement.cs
+++ Assets/Scripts/LevelManagement.cs
@@ -6,9 +6,23 @@ using UnityEngine.SceneManagement;
 
 public class LevelManagement : MonoBehaviour
 {
+    public GameObject display1;
+    public GameObject display2;
 
     public void goToMainMenu()
     {
         SceneManager.LoadScene("Menu");
     }
+
+    public void goToPostLevel()
+    {
+        SceneManager.LoadScene("PostLevel");
+    }
+
+    public void switchDisplays()
+    {
+        display1.SetActive(false);
+        display2.SetActive(true);
+    }
+
 }
